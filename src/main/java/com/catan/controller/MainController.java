fde233224ec947bfec7333727e774efb4b30e60f
Chruisi <chruisi4@gmail.com>
2025-07-13 21:27:30 +0200

    Direkter Link auf Anleitung

--- src/main/java/com/catan/controller/MainController.java
+++ src/main/java/com/catan/controller/MainController.java
@@ -424,7 +424,8 @@ public class MainController implements Initializable {
     @FXML
     private void handleAboutCatan() {
         if (hostServices != null) {
-            hostServices.showDocument("https://www.catan.de/catan-verstehen/spielregeln");
+            //hostServices.showDocument("https://www.catan.de/catan-verstehen/spielregeln");
+            hostServices.showDocument("https://www.catan.de/sites/default/files/2025-03/4002051684655_CAT_NE_Basis34_Manual_DE_web.pdf");
             System.err.println("HostServices nicht gesetzt!");
         }
     }
